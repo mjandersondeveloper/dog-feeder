6bd65ddc4f04be4aefd57adce541c9fe41ed8cf2
Marcus Anderson <manderson332@gatech.edu>
2026-05-10 22:02:34 -0400

    converting app to pwa and adding new icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/dog-feeder/'
-})
+  base: "/dog-feeder/",
+
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Dog Feeder",
+        short_name: "DogFeeder",
+        description: "Dog feeding reminder app",
+        theme_color: "#111827",
+        background_color: "#111827",
+        display: "standalone",
+        start_url: "/dog-feeder/",
+
+        icons: [
+          {
+            src: "/dog-feeder/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/dog-feeder/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
